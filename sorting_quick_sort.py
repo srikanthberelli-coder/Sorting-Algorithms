99b32e5d58be61a87656e3fef042cab3eff00cc8
srikanthberelli-coder <srikanthberelli@gmail.com>
2025-08-30 18:34:51 +0530

    Add files via upload
    quick sort program using arrays

---
+++ sorting_quick_sort.py
@@ -0,0 +1,11 @@
+def quick_sort(arr):
+  if len(arr)<=1:
+    return arr
+  pivot = arr[len(arr)//2]
+  left = [x for x in arr if x<pivot]
+  middle =[x for x in arr if x==pivot]
+  right = [x for x in arr if x>pivot]
+  return quick_sort(left) + middle + quick_sort(right)
+
+arr = [5,3,8,4,2,7,1]
+print("sorted:",quick_sort(arr))
