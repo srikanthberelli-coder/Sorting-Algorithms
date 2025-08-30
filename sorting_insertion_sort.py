f5e1368cc92dc2a9bbc0e8464ed5ffa2a360bb89
srikanthberelli-coder <srikanthberelli@gmail.com>
2025-08-30 18:32:54 +0530

    Add files via upload
    insertion sort using array

---
+++ sorting_insertion_sort.py
@@ -0,0 +1,13 @@
+def insertion_sort(arr):
+  n = len(arr)
+  for i in range(1,n):
+    key = arr[i]
+    j = i-1
+    while j >= 0 and arr[j] > key:
+      arr[j+1] = arr[j]
+      j -= 1
+    arr[j+1] = key
+  return arr
+
+arr = [5,3,4,1,2]
+print(insertion_sort(arr))
